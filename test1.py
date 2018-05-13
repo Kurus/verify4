43e3d99d39e769eeb3ef7c1ef2e80d4a876e024d
Pradeep <deep0kathir@gmail.com>
2018-05-13 19:47:26 +0530

    reodering

--- test1.py
+++ test1.py
@@ -7,7 +7,7 @@ ker = 64
 sq_ker = 16
 pool_en = 0
 av_pool_en = 0
-random = 0 #TODO
+random = 1 #TODO
 sq_rep = 0 # repete squze kernl for last layer
 
 #######################         Input image
@@ -37,45 +37,59 @@ for d in range(0,dep):
             f_in_c.write(str(lis)[1:-1]+'\n')
             f_in_c_b.write(bytearray(lis))
 ########################        expand kernels 
-ker_l_1 = np.arange(ker*dep, dtype='uint8').reshape((ker,dep))
+# ker_l_1 =np.zeros(ker*dep, dtype='uint8').reshape((ker,dep))
+ker_l_1 =np.random.randint(100,size=ker*dep, dtype='uint8').reshape((ker,dep))
 # print(ker_l_1);print("________")
 f_k_1 = open("ker_1x1.txt","w")
 f_k_1_b = open("ker_1x1.bin","wb")
+# for z in range(0,dep):
+#     lis = ker_l_1[:,z]
+#     f_k_1_b.write(bytearray(lis))
+#     f_k_1.write(str(lis)[1:-1]+'\n')
 for z in range(0,dep):
     for x in range(0,ker,8):
-        lis = ker_l_1[x+4:x+8,z]
-        f_k_1_b.write(bytearray(lis))
-        f_k_1.write(str(lis)[1:-1]+'\n')
-        lis = ker_l_1[x:x+4,z]
+        lis = ker_l_1[x:x+4,z][::-1]
         f_k_1_b.write(bytearray(lis))
         f_k_1.write(str(lis)[1:-1]+'\n')
 
-ker_l_3 = np.arange(ker*dep*9, dtype='uint8').reshape((ker,dep,9))
-# print(ker_l_3[0,0,:]);print("________")
+        lis = ker_l_1[x+4:x+8,z][::-1]
+        f_k_1_b.write(bytearray(lis))
+        f_k_1.write(str(lis)[1:-1]+'\n')
+
+# ker_l_3 = np.asarray(list(range(0,9))*ker*dep, dtype='uint8').reshape((ker,dep,9))
+# ker_l_3 = np.ones(ker*dep*9, dtype='uint8').reshape((ker,dep,9))
+ker_l_3 = np.random.randint(100,size=ker*dep*9, dtype='uint8').reshape((ker,dep,9))
+# print(ker_l_3[0:8,0,:]);print("________")
 f_k_3 = open("ker_3x3.txt","w")
 f_k_3_b = open("ker_3x3.bin","wb")
 # for m in range(0,dim): # repet 3x3 kernel # removed repeating
+# ordering 78 345 012 ## 6
 for z in range(0,dep):
     lis = ker_l_3[:,z,:]
     for x in range(0,ker,8):
         for a in range(0,8):
-            eig = lis[x+a,0:8]
+            eig = lis[x+a,[7,8,3,4,5,0,1,2]] #reversed
             f_k_3_b.write(bytearray(eig))
             f_k_3.write(str(eig)[1:-1]+'\n')
-        nin = lis[x:x+8,-1].flatten()[::-1] #reversed
+        nin = lis[x:x+8,6].flatten() #no reversed 6 means 
         f_k_3_b.write(bytearray(nin))
         f_k_3.write(str(nin)[1:-1]+'\n')
+
 ########################        exapnd bias
-bis_1 = np.arange(ker,dtype='uint8')
-bis_3 = np.arange(ker,dtype='uint8')
+# bis_1 = np.arange(ker,dtype='uint8')
+bis_1 = np.random.randint(low = 0, high = 255, size=ker,dtype='uint8')
+# bis_3 = np.ones(ker,dtype='uint8')
+bis_3 = np.random.randint(low = 0, high = 255, size=ker,dtype='uint8')
 b_bis = open("bias.txt","w")
 b_bis_b = open("bias.bin","wb")
 # print(bis_1)
 for i in range(0,ker,4):
-    b_bis.write(str(bis_3[i:i+4])[1:-1]+'\n')
-    b_bis.write(str(bis_1[i:i+4])[1:-1]+'\n')
-    b_bis_b.write(bytearray(bis_3[i:i+4]))
-    b_bis_b.write(bytearray(bis_1[i:i+4]))
+    lis_b3 = bis_3[i:i+4][::-1] # reverse
+    lis_b1 = bis_1[i:i+4][::-1] #reverst
+    b_bis.write(str(lis_b1)[1:-1]+'\n')
+    b_bis.write(str(lis_b3)[1:-1]+'\n')
+    b_bis_b.write(bytearray(lis_b1))
+    b_bis_b.write(bytearray(lis_b3))
 
 #######################        expand convolution
 out_1 = np.zeros(ker*dep*dim*dim, dtype='uint8').reshape((ker,dep,dim,dim))
@@ -197,8 +211,9 @@ else:
 
 ########################   squ kernel
 if random == 0:
-    sq_ker_l = np.arange(sq_ker*dep, dtype='uint8').reshape((sq_ker,dep))
+    sq_ker_l = np.ones(sq_ker*dep, dtype='uint8').reshape((sq_ker,dep))
 else:
+    # sq_ker_l = np.ones(sq_ker*dep, dtype='uint8').reshape((sq_ker,dep))
     sq_ker_l = np.random.randint(low = 0, high = 255, size = (sq_ker,dep), dtype='uint8')
 
 sq_k_1 = open("sq_ker.txt","w")
@@ -212,23 +227,24 @@ if(sq_rep == 1):
 for r in range(0,rep_no):
     for x in range(0,sq_ker):
         for z in range(0,dep_h,8):
-            lis = sq_ker_l[x,z+dep_h:z+dep_h+8]#kerle of 3x3 part
+            lis = sq_ker_l[x,z+dep_h:z+dep_h+8][::-1]#kerle of 3x3 part # reverse added
             sq_k_1.write(str(lis)[1:-1]+'\n')
             sq_k_1_b.write(bytearray(lis))
 
-            lis = sq_ker_l[x,z:z+8]
+            lis = sq_ker_l[x,z:z+8][::-1] #reverse added
             sq_k_1.write(str(lis)[1:-1]+'\n')
             sq_k_1_b.write(bytearray(lis))
     
 
 #######################    squ bias
-sq_bis_1 = np.arange(sq_ker,dtype='uint8')
+sq_bis_1 = np.random.randint(10,size=sq_ker,dtype='uint8')
+# sq_bis_1 = np.zeros(sq_ker,dtype='uint8')
 f_sq_bis = open("sq_bias.txt","w")
 f_sq_bis_b = open("sq_bias.bin","wb")
 
 for x in range(0,sq_ker,8):
     lis = sq_bis_1[x:x+8]
-    lis = lis[::-1] #reverse
+    # lis = lis[::-1] #reverse
     f_sq_bis.write(str(lis)[1:-1]+'\n')
     f_sq_bis_b.write(bytearray(lis))
 
@@ -263,7 +279,8 @@ f_sq_out_1_c = open("sq_out_c.txt","w")
 for r in range(0,sq_ker):
     for d in range(0,dim_sq):
         lis = sq_out[r,d,:]
-        f_sq_out_1.write(str(lis)[1:-1]+'\n')
+        lisStr = ' '.join(map(str,list(lis)))
+        f_sq_out_1_c.write(lisStr+'\n')
 
 ########################     avg pool
 if av_pool_en == 1:
