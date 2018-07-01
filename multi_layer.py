e9aab8d75d655f2f61fadd1192f0bdbe5123372a
Pradeep <deep0kathir@gmail.com>
2018-07-01 13:02:44 +0530

    removed ugly strid 2 implementation

--- multi_layer.py
+++ multi_layer.py
@@ -4,17 +4,17 @@ import numpy as np
 from scipy import signal as sg
 import os
 
-dim = 220
+dim = 7
 dim_p=dim + 2
 dep = 3
 ker_list = [64,64, 64, 128, 128, 192, 192, 256, 256]
 sq_ker_list = [16,16, 32, 32, 48, 48, 64, 64, 1000]
-pool_en_list = [1,0, 1, 0, 1, 0, 0, 0, 0]
+pool_en_list = [1,0, 0, 0, 0, 0, 0, 0, 0]
 av_pool_en_list = [0,0,0,0, 0, 0, 0, 0, 1]
 stride2_en_list = [1,0,0,0, 0, 0, 0, 0, 0]
 sq_rep_list = [0,0,0,0, 0, 0, 0, 0, 0] # repete squze kernl for last layer
 random = 0 #TODO
-num_layer = 9
+num_layer = 1
 
 
 final_out = []
@@ -224,9 +224,9 @@ for cur_ly in range(0,num_layer):
     print("input layer");print(in_l[:,:,0]); 
     ########################        expand kernels 
     # ker_l_1 = np.zeros(ker*dep, dtype='uint8').reshape((ker,dep))
-    # ker_l_1 = np.full(ker*dep,0,dtype='uint8').reshape((ker,dep))
+    ker_l_1 = np.full(ker*dep,0,dtype='uint8').reshape((ker,dep))
     # ker_l_1[0,0]=60
-    ker_l_1 = np.random.randint(low = 0, high = 255, size = (ker*dep),dtype='uint8').reshape((ker,dep))
+    # ker_l_1 = np.random.randint(low = 0, high = 255, size = (ker*dep),dtype='uint8').reshape((ker,dep))
     if stride2_en == 1:# for stride 2 exp 1 is zero
         ker_l_1 = np.zeros(ker*dep, dtype='uint8').reshape((ker,dep))
     print("kernel1");print(ker_l_1)
@@ -243,8 +243,9 @@ for cur_ly in range(0,num_layer):
         # f_k_1.write(str(lis)[1:-1]+'\n')
 
     # ker_l_3 = np.arange(ker*dep*9, dtype='uint8').reshape((ker,dep,9))
-    # ker_l_3 = np.full(ker*dep*9,0,dtype='uint8').reshape((ker,dep,9))
-    ker_l_3 = np.random.randint(low = 0, high = 255, size = (ker,dep,9),dtype='uint8').reshape((ker,dep,9))
+    ker_l_3 = np.full(ker*dep*9,0,dtype='uint8').reshape((ker,dep,9))
+    ker_l_3[0,0,4]=60
+    # ker_l_3 = np.random.randint(low = 0, high = 255, size = (ker,dep,9),dtype='uint8').reshape((ker,dep,9))
     # print(ker_l_3[0,0,:]);print("________")
     # f_k_3 = open("ker_3x3.txt","w")
     f_k_3_b = open("ker_3x3"+"_"+str(cur_ly)+".bin","wb")
@@ -263,7 +264,7 @@ for cur_ly in range(0,num_layer):
     ker_l_1 = b2dv(ker_l_1)
     ker_l_3 = b2dv(ker_l_3)
     print("expand kernel 1");print(ker_l_1[0,:])
-    print("expand kernel 3");print(ker_l_3[11, 0,:])
+    print("expand kernel 3");print(ker_l_3[0, 0,:])
     ########################        exapnd bias
     bis_1 = np.full(ker,0x00,dtype='uint8') #one
     # bis_1 = np.random.randint(low = 0, high = 255, size = (ker),dtype='uint8')
@@ -286,9 +287,9 @@ for cur_ly in range(0,num_layer):
     print("exp1 bias");print(bis_1)
     print("exp3 bias");print(bis_3)
     #######################        expand convolution
-    out_1 = np.zeros(ker*dep*dim*dim, dtype='float64').reshape((ker,dep,dim,dim))
     #stride 2 means 3x3 conv only
-    if stride2_en==0:##for stride enable this will be zero. for stride 2 we should move 2
+    out_1 = np.zeros(ker*dep*dim*dim, dtype='float64').reshape((ker,dep,dim,dim))
+    if stride2_en==0:##for stride enable this will be zero. see  exp 3 stride
         for k in range(0,ker):
             for l in range(0,dep):
                 res = sg.convolve(in_l[:,:,l],[[ker_l_1[k,l]]] , "valid").astype(float)
@@ -332,6 +333,7 @@ for cur_ly in range(0,num_layer):
         else:
             o_dim= dim //2
         out_3 = np.zeros(ker*dep*o_dim*o_dim, dtype='float64').reshape((ker,dep,o_dim,o_dim))
+        out_1 = np.zeros(ker*dep*dim*dim, dtype='float64').reshape((ker,dep,dim,dim)) # output of exp 1 is zeros
         for k in range(0,ker):
             for l in range(0,dep):
                 kk = ker_l_3[k,l]
@@ -349,6 +351,8 @@ for cur_ly in range(0,num_layer):
                         ll = dq(dq(l1) + dq(ll[2]) )
                         out_3[k,l,a//2,b//2]=ll
         dim=o_dim
+
+
     print("exp3 out bef add")
     print(out_3[0,0,:,:])
     # out_3 = np.arange(ker*dep*dim*dim, dtype='uint8').reshape((ker,dep,dim,dim))
@@ -467,21 +471,15 @@ for cur_ly in range(0,num_layer):
 
     ########################## squeeze
     sq_in=[] # dep*dim*dim
-    dep = ker*2 # TODO firs layer no ned 2
-    if stride2_en==1:
-        dep = ker # firs layer no ned 2
+    dep = ker*2 
+    # if stride2_en==1:
+    #     dep = ker # added zero for exp
 
     if pool_en == 1: # ########TODO add first layer heere
         dim_sq = dim_o
-        if stride2_en==1:
-            sq_in = pool_3
-        else:
-            sq_in = np.concatenate((pool_1, pool_3), axis=0)
+        sq_in = np.concatenate((pool_1, pool_3), axis=0)
     else:
-        if stride2_en==1:
-            sq_in=out_3
-        else:
-            sq_in = np.concatenate((out_1, out_3), axis=0)
+        sq_in = np.concatenate((out_1, out_3), axis=0)
         dim_sq = dim
 
     # print(out_1[31,:,:])
@@ -493,16 +491,11 @@ for cur_ly in range(0,num_layer):
     if random == 0:
         #sq_ker_l = np.full(sq_ker*dep,65,dtype='uint8').reshape((sq_ker,dep))
         # sq_ker_l = np.random.randint(low=0, high=255, size = (sq_ker*dep),dtype='uint8').reshape((sq_ker,dep))
-        # sq_ker_l = np.full(sq_ker*dep,0, dtype='uint8').reshape((sq_ker,dep))
-        # sq_ker_l[0,0]=60
-        sq_ker_l = np.random.randint(low = 0, high = 255, size = (sq_ker,dep), dtype='uint8')
+        sq_ker_l = np.full(sq_ker*dep,0, dtype='uint8').reshape((sq_ker,dep))
+        sq_ker_l[0,dep//2]=60
+        # sq_ker_l = np.random.randint(low = 0, high = 255, size = (sq_ker,dep), dtype='uint8')
     else:
         sq_ker_l = np.random.randint(low = 0, high = 255, size = (sq_ker,dep), dtype='uint8')
-    if stride2_en==1:
-        tmp = np.full(sq_ker*dep,0, dtype='uint8').reshape((sq_ker,dep))
-        sq_ker_l_write = np.append(sq_ker_l,tmp,axis=1)
-    else:
-        sq_ker_l_write = sq_ker_l
 
     # sq_k_1 = open("sq_ker.txt","w")
     sq_k_1_b = open("sq_ker"+"_"+str(cur_ly)+".bin","wb")
@@ -545,7 +538,7 @@ for cur_ly in range(0,num_layer):
             sq_out[k,l,:,:]=dqv(res)
 
     print("squ input before add")
-    inkk=0
+    inkk=dep//2
     print("layer " + str(inkk))
     print(sq_in[inkk,:,:])
     print("kernel")
